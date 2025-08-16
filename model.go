931cba4ef6829a7997d64dc372dd639cb64f1631
okkuweb <okkuweb@protonmail.com>
2025-08-16 23:04:06 +0900

    Added enemy AI and damage, part-6

--- model.go
+++ model.go
@@ -1,6 +1,11 @@
 package main
 
-import "codeberg.org/anaseto/gruid"
+import (
+	"sort"
+
+	"codeberg.org/anaseto/gruid"
+	"codeberg.org/anaseto/gruid/paths"
+)
 
 type model struct {
 	grid   gruid.Grid // drawing grid
@@ -16,9 +21,14 @@ func (m *model) Update(msg gruid.Msg) gruid.Effect {
 		// Initialize map
 		size := m.grid.Size() // map size: for now the whole window
 		m.game.Map = NewMap(size)
+		m.game.PR = paths.NewPathRange(gruid.NewRange(0, 0, size.X, size.Y))
 		// Initialize entities
 		m.game.ECS = NewECS()
 		m.game.ECS.PlayerID = m.game.ECS.AddEntity(NewPlayer(), m.game.Map.RandomFloor())
+		m.game.ECS.Fighter[m.game.ECS.PlayerID] = &fighter{
+			HP: 30, MaxHP: 30, Power: 5, Defense: 2,
+		}
+		m.game.ECS.Name[m.game.ECS.PlayerID] = "you"
 		m.game.UpdateFOV()
 		m.game.SpawnMonsters()
 	case gruid.MsgKeyDown:
@@ -47,7 +57,6 @@ func (m *model) updateMsgKeyDown(msg gruid.MsgKeyDown) {
 		m.action = action{Type: ActionBump, Delta: pdelta.Shift(0, -1)}
 	case "l":
 		m.action = action{Type: ActionBump, Delta: pdelta.Shift(1, 0)}
-
 	case "y":
 		m.action = action{Type: ActionBump, Delta: pdelta.Shift(-1, -1)}
 	case "u":
@@ -56,6 +65,9 @@ func (m *model) updateMsgKeyDown(msg gruid.MsgKeyDown) {
 		m.action = action{Type: ActionBump, Delta: pdelta.Shift(-1, 1)}
 	case "n":
 		m.action = action{Type: ActionBump, Delta: pdelta.Shift(1, 1)}
+	case ".":
+		m.action = action{Type: ActionWait}
+
 
 	case gruid.KeyEscape, "Q":
 		m.action = action{Type: ActionQuit}
@@ -77,15 +89,22 @@ func (m *model) Draw() gruid.Grid {
 		}
 		m.grid.Set(it.P(), c)
 	}
-	// We draw the entities.
-	for i, e := range g.ECS.Entities {
+	// We sort entity indexes using the render ordering.
+	sortedEntities := make([]int, 0, len(g.ECS.Entities))
+	for i := range g.ECS.Entities {
+		sortedEntities = append(sortedEntities, i)
+	}
+	sort.Slice(sortedEntities, func(i, j int) bool {
+		return g.ECS.RenderOrder(sortedEntities[i]) < g.ECS.RenderOrder(sortedEntities[j])
+	})
+	// We draw the sorted entities.
+	for _, i := range sortedEntities {
 		p := g.ECS.Positions[i]
 		if !g.Map.Explored[p] || !g.InFOV(p) {
 			continue
 		}
 		c := m.grid.At(p)
-		c.Rune = e.Rune()
-		c.Style.Fg = e.Color()
+		c.Rune, c.Style.Fg = g.ECS.Style(i)
 		m.grid.Set(p, c)
 	}
 	return m.grid
--- todo.md
+++ todo.md
@@ -2,3 +2,4 @@
 - Make the field of view visible for the player. In the graphical edition it displays as a different hue, but I had to remove it on for the terminal version and have to figure out how to implement it again. Refer to part-4 for hints.
 - Write the logs to a separate box instead of all over the screen
 - Make the FoV into a box instead of a weird... I dunno-shape
+- The enemy pathfinding is made for 4 directional movement. Make it 8 directional>
